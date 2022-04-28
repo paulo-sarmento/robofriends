b60934fdb24c539db8009fbc62bc397c166a5e2c
Paulo Almeida <paulocomy1408@gmail.com>
2022-04-27 22:30:37 -0400

    Updates

---
+++ precache-manifest.88c45b4ef07574c490e16823c3f57319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0994d25bf361451262b",
+    "url": "/robofriends/static/css/main.350a26b4.chunk.css"
+  },
+  {
+    "revision": "d0994d25bf361451262b",
+    "url": "/robofriends/static/js/main.d0994d25.chunk.js"
+  },
+  {
+    "revision": "11ed4885cbaf6234abf7",
+    "url": "/robofriends/static/css/1.df173417.chunk.css"
+  },
+  {
+    "revision": "11ed4885cbaf6234abf7",
+    "url": "/robofriends/static/js/1.11ed4885.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5769810790d4c0b1a4fad223d29c48b5",
+    "url": "/robofriends/index.html"
+  }
+];
